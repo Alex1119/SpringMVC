b1f701849c86ab33f67c85815101762fd7d7ee5c
Alex Xing <916000345@qq.com>
2017-08-30 09:32:46 +0800

    add httpclient

--- SpringMVC/src/main/java/Controller/MainController.java
+++ SpringMVC/src/main/java/Controller/MainController.java
@@ -6,6 +6,9 @@ import Entity.UserDetailEntity;
 import Model.Response;
 import Model.DTO_Input_Register;
 import Model.DTO_Output_UserDetail;
+import Utils.HttpClientUtil;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.dozer.Mapper;
@@ -15,6 +18,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 @AuthorizeAnnotation
 @Controller
@@ -41,6 +47,18 @@ public class MainController extends BaseController{
                                                          @ApiParam(value = "参数名") @RequestBody String viewModel){
         UserDetailEntity entity = _UserRepository.GetUserDetail(viewModel);
         DTO_Output_UserDetail result = DTO_Output_UserDetail.ConvertFrom(_Mapper, entity);
+
+        Map params = new TreeMap<String, Object>();
+        params.put("AccountId", "26d60997-f9b5-42fb-b36d-8a44a430ac8a");
+        Gson gson = new GsonBuilder().serializeNulls().enableComplexMapKeySerialization().create();
+
+        String postResult = HttpClientUtil.getInstance().httpPost("http://hzd6tx24d2/ZSTJ/api/V3/HealthTrace/HealthTraceList",
+                gson.toJson(params),
+                new HashMap<String, String>(){{
+                    put("Content-Type","application/json; charset=UTF-8" );
+                    put("Accept","application/json" );
+                    put("Authorization","123" );
+                }});
         return ResponseData(result);
     }
 
